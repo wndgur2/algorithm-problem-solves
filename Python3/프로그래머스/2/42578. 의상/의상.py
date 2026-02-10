c03f3e6f4676b29ac8cc1674276be61cc7f776b8
Lee Junghyeok <wndgur76@naver.com>
2026-02-10 18:59:47 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,14 @@
+def solution(clothes):
+    hash_map = {}
+
+    for c, t in clothes:
+        print(t)
+        if t in hash_map:
+            hash_map[t] += 1
+        else:
+            hash_map[t] = 1
+    
+    answer = 1
+    for n in hash_map:
+        answer *= hash_map[n]+1
+    return answer - 1
