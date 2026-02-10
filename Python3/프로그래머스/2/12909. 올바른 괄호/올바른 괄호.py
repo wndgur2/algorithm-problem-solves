d7f54bda0441c1a431f2af31e1e66bff5fcd89f9
Lee Junghyeok <wndgur76@naver.com>
2026-02-10 19:00:58 +0900

    [level 2] Title: 올바른 괄호, Time: 6.85 ms, Memory: 9.31 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = True
+    openNum = 0
+    
+    for c in s:
+        if(c=='('):
+            openNum += 1
+        else:
+            if(openNum == 0):
+                return False
+            else:
+                openNum -= 1
+    if(openNum > 0):
+        return False
+    
+    return True
