eb4e1ed1f88c961862b893ccdb60714ded3f0e92
Lee Junghyeok <wndgur76@naver.com>
2026-02-10 18:52:16 +0900

    [level 3] Title: 가장 먼 노드, Time: 36.76 ms, Memory: 23.5 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,27 @@
+'''
+    이건 BFS?
+'''
+from collections import deque
+def solution(n, edge):
+    answer = 0
+    graph = [[] for _ in range(n+1)]
+    for l in edge:
+        graph[l[0]].append(l[1])
+        graph[l[1]].append(l[0])
+    
+    visited = set({1})
+    q = deque([[1, 0]])
+    maxC = 0
+    while q:
+        node, cost = q.popleft()
+        for l in graph[node]:
+            if not l in visited:
+                q.append([l, cost+1])
+                visited.add(l)
+        if cost==maxC:
+            answer += 1
+        if cost>maxC:
+            maxC = cost
+            answer = 1
+        
+    return answer
