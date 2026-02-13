ab1b0aefc67049cb0b5e61db6e7bb8c9a6a1a747
Lee Junghyeok <wndgur76@naver.com>
2026-02-13 12:34:02 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 92 ms, Memory: 9396 KB -BaekjoonHub

---
+++ node.js/백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const lines = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+// const lines = fs.readFileSync('inputs').toString().trim().split('\n')
+const N = Number(lines[0])
+const K = Number(lines[2])
+const amounts = lines[1].split(' ').map(Number)
+const sum = amounts.reduce((p,c)=>p+c, 0)
+
+// K개를 뽑았는데 모두 같은 색일 확률
+// = 각 색을 모두 뽑을 확률을 더한 것
+
+// 1에서 각 색을 1~min(k-1, n)개 뽑을 확률을 뺀 것?
+// nCk / 0~min(k, n)
+
+let answer = 0
+
+for(let c=0; c<N; c++){
+  const n = amounts[c]
+  if(n<K) continue
+  let perc = 1
+  let cs = sum
+  let cn = n
+  let ck = K
+  while(ck>0){
+    perc *= cn/cs
+    cn--;ck--;cs--;
+  }
+  answer += perc
+}
+
+console.log(answer.toFixed(12))
+
+function factorial(n){
+  if(n<=1) return 1
+  return n*factorial(n-1)
+}
