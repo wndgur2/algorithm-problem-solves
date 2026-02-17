0648a6a4ced416151ada44f84b6d16ebc08ce8ab
Lee Junghyeok <wndgur76@naver.com>
2026-02-17 18:16:15 +0900

    [Silver II] Title: 침투, Time: 208 ms, Memory: 33624 KB -BaekjoonHub

---
+++ node.js/백준/Silver/13565. 침투/침투.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const lines = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+// const lines = fs.readFileSync('inputs').toString().trim().split('\n')
+const [M, N] = lines.shift().split(' ').map(Number)
+const board = lines.map(s => Array.from(s).map(v=>Boolean(Number(v))))
+
+let flag = false
+const ds = [[1, 0], [0, 1], [0, -1], [-1, 0]]
+const visited = Array(M).fill(0).map(v=>Array(N).fill(false))
+
+for(let i=0; i<N; i++)
+  dfs(0, i)
+
+console.log(flag?'YES':'NO')
+
+
+function dfs(y, x){
+  if(board[y][x]) return
+  if(y===M-1) return flag = true
+  if(flag) return
+  if(visited[y][x]) return
+  visited[y][x] = true
+
+  for(let di=0; di<4; di++){
+    const [dy, dx] = ds[di]
+    const ny = y + dy
+    const nx = x + dx
+    if(ny<0 || nx<0 || ny>=M || nx>=N) continue
+    if(visited[ny][nx]) continue
+    if(board[ny][nx]) continue
+    dfs(ny, nx)
+  }
+}
