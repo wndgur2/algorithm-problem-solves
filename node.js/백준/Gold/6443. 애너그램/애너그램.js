a686b17d72e99e12c6bfcffa85925ce13ecc126b
Lee Junghyeok <wndgur76@naver.com>
2026-02-17 19:04:46 +0900

    [Gold V] Title: 애너그램, Time: 364 ms, Memory: 58776 KB -BaekjoonHub

--- node.js/백준/Gold/6443. 애너그램/애너그램.js
+++ node.js/백준/Gold/6443. 애너그램/애너그램.js
@@ -2,36 +2,32 @@ const fs = require('fs')
 const lines = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 const N = Number(lines[0])
 const res = []
+
 for(let i=0; i<N; i++){
   const word = lines[1+i]
-  const visited = new Set()
-  const leftCharCounts = new Map()
-  for(let j=0; j<word.length; j++){
-    const char = word[j]
-    if(!leftCharCounts.has(char)) leftCharCounts.set(char, 0)
-    leftCharCounts.set(char, leftCharCounts.get(char)+1)
+  const wordArray = word.split('')
+  wordArray.sort()
+
+  const charCounts = {}
+  for(let j=0; j<wordArray.length; j++){
+    const char = wordArray[j]
+    charCounts[char] = charCounts[char]? charCounts[char]+1:1
   }
 
-  dfs(Array(word.length).fill(''), 0, leftCharCounts, visited)
+  dfs(charCounts, "")
 
-  function dfs(charArray, idx){
-    let key = charArray.slice(0, idx).join('')
-    if(visited.has(key)) return
-    visited.add(key)
+  function dfs(charCounts, string){
+    if(string.length==wordArray.length) 
+      return res.push(string)
   
-    if(idx==word.length) {res.push(charArray.join('')); return}
-  
-    for(let i=0; i<word.length; i++){
-      const c = word[i]
-      if(leftCharCounts.get(c)===0) continue
-      leftCharCounts.set(c, leftCharCounts.get(c)-1)
-      charArray[idx] = c
-      dfs(charArray, idx+1)
-      leftCharCounts.set(c, leftCharCounts.get(c)+1)
+    for(const char in charCounts){
+      if(charCounts[char]>0){
+        charCounts[char]--
+        dfs(charCounts, string+char)
+        charCounts[char]++
+      }
     }
   }
 }
-res.sort()
-res.sort((a,b)=>a.length-b.length)
 
 console.log(res.join('\n'))
