a2cb0928c7274c7e19dc6c97c86528f2fee465b1
Lee Junghyeok <wndgur76@naver.com>
2026-02-18 14:01:21 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 380 ms, Memory: 39504 KB -BaekjoonHub

--- node.js/백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.js
+++ node.js/백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.js
@@ -1,23 +1,25 @@
 const fs = require('fs')
 const lines = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+// const lines = fs.readFileSync('inputs').toString().trim().split('\n')
 const [N, S] = lines[0].split(' ').map(Number)
 const nums = lines[1].split(' ').map(Number)
 
 const mid = Math.floor(nums.length/2)
-const combi = {0:1}
+const combi = new Map()
+combi.set(0, 1)
 
 let ans = S===0?-1:0
 
 dfs(0, 0, mid)
 dfs2(mid, 0, N)
 
-ans += combi[S]?combi[S]:0
+ans += combi.has(S)?combi.get(S):0
 console.log(ans)
 
 function dfs(idx, prevSum, maxIdx){
   if(idx===maxIdx) return
   const newSum = prevSum + nums[idx]
-  combi[newSum] = combi[newSum]? combi[newSum]+1: 1
+  combi.set(newSum, combi.has(newSum)? combi.get(newSum)+1: 1)
 
   dfs(idx+1, newSum, maxIdx)
   dfs(idx+1, prevSum, maxIdx)
@@ -26,8 +28,8 @@ function dfs(idx, prevSum, maxIdx){
 function dfs2(idx, prevSum, maxIdx){
   if(idx===maxIdx) return
   const newSum = prevSum + nums[idx]
-  if(combi[S-newSum]){
-    ans += combi[S-newSum]
+  if(combi.has(S-newSum)){
+    ans += combi.get(S-newSum)
   }
 
   dfs2(idx+1, newSum, maxIdx)
