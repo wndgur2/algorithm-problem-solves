7ab1913c29af0aa202d0e4546066152a7d72f3a8
Lee Junghyeok <wndgur76@naver.com>
2026-03-02 15:06:43 +0900

    [Gold III] Title: 정육점, Time: 352 ms, Memory: 49596 KB -BaekjoonHub

--- node.js/백준/Gold/2258. 정육점/정육점.js
+++ node.js/백준/Gold/2258. 정육점/정육점.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-// const lines = fs.readFileSync('inputs').toString().trim().split('\n')
 const lines = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 const [N, M] = lines.shift().split(' ').map(Number)
 const ms = lines.map(v=>v.split(' ').map(Number))
@@ -10,33 +9,30 @@ let wSum = 0
 let sameCostCount=1
 let cost = 0
 let minCost = 2200000000
-let flag = false
 for(let i=0; i<N; i++){
   const [w, c] = ms[i]
   wSum += w
 
   if(c>cost){
     // 가격이 증가하는 일반적인 상황
-    cost = c
     sameCostCount=1
+    cost = c
 
     if(wSum >= M){
-      if(cost<minCost){
-        flag = true
-        minCost = cost
+      if(c<minCost){
+        minCost = c
       }
     }
   } else{
     // 같은 가격의 고기 덩어리
     sameCostCount++
     if(wSum >= M){
-      const tc = cost * sameCostCount
+      const tc = c * sameCostCount
       if(tc<minCost){
-        flag = true
         minCost = tc
       }
     }  
   }
 }
 
-console.log(flag?minCost:-1)
+console.log(minCost<2200000000?minCost:-1)
