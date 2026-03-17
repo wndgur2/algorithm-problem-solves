d4e0980d44d1d953cb44c81dcce2e518bf8c9fad
Lee Junghyeok <wndgur76@naver.com>
2026-03-17 10:22:39 +0900

    [Silver II] Title: 영재의 시험, Time: 248 ms, Memory: 10080 KB -BaekjoonHub

---
+++ node.js/백준/Silver/19949. 영재의 시험/영재의 시험.js
@@ -0,0 +1,21 @@
+let fs = require('fs')
+let answers = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+let res = 0
+
+function dfs(index, score, prev=0, prevDeep=0){
+  if(index===10) {
+    if(score>=5) res++
+    return
+  }
+
+  for(let na=1; na<=5; na++){
+    if(na==prev && na==prevDeep) continue
+    const ns = answers[index]==na?score+1:score
+    dfs(index+1, ns, na, prev)
+  }
+}
+
+dfs(0, 0)
+
+console.log(res)
