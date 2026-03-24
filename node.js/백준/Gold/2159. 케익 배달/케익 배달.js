1f1f6e6ab6a9df1dc3f41547fa021cfaefbbf410
Lee Junghyeok <wndgur76@naver.com>
2026-03-24 13:59:48 +0900

    [Gold III] Title: 케익 배달, Time: 440 ms, Memory: 72752 KB -BaekjoonHub

--- node.js/백준/Gold/2159. 케익 배달/케익 배달.js
+++ node.js/백준/Gold/2159. 케익 배달/케익 배달.js
@@ -1,20 +1,21 @@
 const MAX_DISTANCE = 10_000_000_000
 
 let fs = require('fs');
-let lines = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-// let lines = fs.readFileSync('inputs').toString().trim().split('\n');
+// let [input1, input2, ...input3] = fs.readFileSync('inputs').toString().trim().split('\n');
+let [input1, input2, ...input3] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const N = Number(lines.shift())
+const N = +input1
+const coords = input3.map(v=>v.split(' ').map(Number))
 
-
-const dp = Array(N+1).fill(0).map(v=>Array(5).fill(MAX_DISTANCE))
 const ds = [[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0]]
 
-let [y, x] = lines.shift().split(' ').map(Number)
+const dp = Array(N+1).fill(0).map(v=>Array(5).fill(MAX_DISTANCE))
 dp[0] = [0,1,1,1,1]
 
+let [y, x] = input2.split(' ').map(Number)
+
 for(let i=1; i<=N; i++){
-  const [ny, nx] = lines[i-1].split(' ').map(Number)
+  const [ny, nx] = coords[i-1]
   for(let di=0; di<5; di++){
     const [cy, cx] = [y+ds[di][0], x+ds[di][1]]
     const oc = dp[i-1][di]
@@ -24,18 +25,10 @@ for(let i=1; i<=N; i++){
 
       if(oc+nc < dp[i][dj]){
         dp[i][dj] = oc+nc
-        // console.log('-----------------------')
-        // console.log(cy, cx)
-        // console.log('to')
-        // console.log(ty, tx)
-        // console.log('cost---')
-        // console.log(oc, nc)
-        // console.log('-----------------------')
       }
     }
   }
   [y, x] = [ny, nx]
-  // console.log(dp[i])
 }
 
 console.log(Math.min(...dp[N]))
